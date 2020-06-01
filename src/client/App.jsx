780a4eb8105fb38244bf3d9db1026ccb96956b5c
YANNIEYEUNG <K_T_DINO@HOTMAIL.COM>
2020-06-01 20:00:49 +0800

    MVP

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React, { useState } from "react";
+import { hot } from "react-hot-loader";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Welcome.
-      </div>
-    );
+function App() {
+  const [todoItems, setItem] = useState([]);
+  const [input, setInput] = useState("");
+
+  function inputChange(event) {
+    console.log(event.target.value);
+    var newInput = event.target.value;
+    setInput(newInput);
   }
+
+  function toAdd(event) {
+    setItem((prevItem) => {
+      return [...prevItem, input];
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <p>
+        {todoItems.map((eachTodo) => {
+          return <li>{eachTodo}</li>;
+        })}
+      </p>
+      <form>
+        <input onChange={inputChange} value={input} name="list"></input>
+        <br />
+        <button onClick={toAdd}>Add</button>
+      </form>
+    </div>
+  );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <div>Welcome!!!!!</div>;
+//   }
+// }
 
 export default hot(module)(App);
